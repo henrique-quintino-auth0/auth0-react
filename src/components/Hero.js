ce1ccccc24419a95da905fe06e3787b5cda0bf35
henrique-quintino-auth0 <85711300+henrique-quintino-auth0@users.noreply.github.com>
2021-07-22 16:45:26 -0300

    Auth0 added label

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 import logo from "../assets/logo.svg";
+import logoAuth0 from "../assets/icon-auth0.svg";
 
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
+    <img className="mb-3 app-logo" src={logoAuth0} alt="Auth0 logo" width="120" />
     <h1 className="mb-4">React.js Sample Project</h1>
 
     <p className="lead">
       This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
+      an SPA, using <a href="https://reactjs.org">React.js</a> and <a href="https://auth0.com">Auth0</a>
     </p>
   </div>
 );
